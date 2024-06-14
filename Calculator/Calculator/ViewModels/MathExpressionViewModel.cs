f3c8851ef2fd7831df01b91959fc1da33cff815e
Sergey Brykulskii <serxio6758sk@gmail.com>
2024-06-14 15:08:19 +0300

    added viewmodel and base button handlers

---
+++ Calculator/Calculator/ViewModels/MathExpressionViewModel.cs
@@ -0,0 +1,55 @@
+﻿using Calculator.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace Calculator.ViewModels;
+
+public partial class MathExpressionViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string _mathExpression = string.Empty;
+    [ObservableProperty]
+    private ObservableCollection<string> _userFunctionsList = [];
+    [ObservableProperty]
+    private ObservableCollection<string> _userVariablesList = [];
+
+    private Dictionary<string, UserFunction> userFuntions = new();
+    private Dictionary<string, (string, string)> userVariables = new();
+
+    public ICommand AddCharCommand { get; private set; }
+    public ICommand DeleteCharCommand { get; private set; }
+    public ICommand ClearEntryCommand { get; private set; }
+    public ICommand AddFunctionCommand { get; private set; }
+    public ICommand AddVariableCommand { get; private set; }
+    public ICommand CalculateExpressionCommand { get; private set; }
+
+    public MathExpressionViewModel()
+    {
+        AddCharCommand = new Command<string>((key) => MathExpression += key);
+
+        DeleteCharCommand = new Command(
+            () => MathExpression = MathExpression.Length > 0 ?
+            MathExpression[..^1] :
+            MathExpression);
+
+        ClearEntryCommand = new Command(() => MathExpression = string.Empty);
+
+        AddFunctionCommand = new Command(() =>
+        {
+            UserFunctionsList.Add(MathExpression);
+            ClearEntryCommand.Execute(this);
+        });
+
+        AddVariableCommand = new Command(() =>
+        {
+            UserVariablesList.Add(MathExpression);
+            ClearEntryCommand.Execute(this);
+        });
+
+        CalculateExpressionCommand = new Command(() =>
+        {
+            ClearEntryCommand.Execute(this);
+        });
+    }
+}
--- Calculator/Calculator/Views/CalculatorMainPage.xaml.cs
+++ Calculator/Calculator/Views/CalculatorMainPage.xaml.cs
@@ -1,9 +1,13 @@
+using Calculator.ViewModels;
+
 namespace Calculator.Views;
 
 public partial class CalculatorMainPage : ContentPage
 {
-	public CalculatorMainPage()
-	{
-		InitializeComponent();
-	}
+    public CalculatorMainPage(MathExpressionViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+        //BindingContext = new MathExpressionViewModel();
+    }
 }
