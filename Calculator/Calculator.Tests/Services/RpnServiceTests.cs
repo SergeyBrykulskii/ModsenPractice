b868aca4c52bded61bc10044061586925e375f54
Oleg Ershov <97069709+OlegErshov@users.noreply.github.com>
2024-06-14 14:23:53 +0300

    Reverse polish notation (#5)
    * Function to convert to reverse polish notion
    
    * Add func to check operators
    
    * Add some tests for rpn function & fux error in rpn function
    
    * Change comma to dot in fractional numbers
    
    * Add some more tests & tests for IsOperator func
    
    * Add changes after pull request for review
    
    * Calculate rpn (#3)
    
    * add rpn calculation
    
    * add tests
    
    * make IsOperator method be a char extension method
    
    * fixed test naming, changed switch statment on switch expression, used another file for char extension class, added blank lines in order to follow AAA pattern
    
    ---------
    
    Co-authored-by: Ksenia Antilevskaya <antikevkun@gmail.com>

--- Calculator/Calculator.Tests/Services/RpnServiceTests.cs
+++ Calculator/Calculator.Tests/Services/RpnServiceTests.cs
@@ -6,6 +6,97 @@ public class RpnServiceTests
 {
 
     [Fact]
+
+    public void CalculateRpn_WithPositiveIntengerNumbersAndMixedOperations_ReturnsCorrectResult1()
+    {
+        var service = new RpnService();
+        var input = "2 5 3 * +";
+
+        var result = service.СalculateRpn(input);
+
+        Assert.Equal(17,result);
+    }
+   
+     [Fact]
+    public void CalculateRpn_WithPositiveIntengerNumbersAndMixedOperations_ReturnsCorrectResult2()
+    {
+        var service = new RpnService();
+        var input = " 5 3 2 - 4 * + ";
+
+        var result = service.СalculateRpn(input);
+
+        Assert.Equal(9, result);
+    }
+
+
+    [Fact]
+    public void CalculateRpn_WithPositiveDoubleNumbersAndMixedOperations_ReturnsCorrectResult()
+    {
+        var service = new RpnService();
+        var input = " 2.5 1.5 + 3.2 * 2 / ";
+
+        var result = service.СalculateRpn(input);
+
+        Assert.Equal(6.4, result);
+    }
+
+    [Fact]
+    public void CalculateRpn_WithNegativeDoubleNumbersAndMixedOperations_ReturnsCorrectResult()
+    {
+        var service = new RpnService();
+        var input = "0 2.5 - 1.3 + 8 /";
+
+        var result = service.СalculateRpn(input);
+
+        Assert.Equal(-0.15, result);
+    }
+
+    [Fact]
+    public void CalculateRpn_WithPositiveIntengerNumbersAndMixedOperations_ReturnsCorrectResult()
+    {
+        var service = new RpnService();
+        var input = "4 3 * 2 5 + / 6 2 1 - / -";
+
+        var result = service.СalculateRpn(input);
+
+        Assert.Equal(-4.285714, result,0.000001);
+       
+    }
+
+    [Fact]
+    public void CalculateRpn_WithDoubleNumbersAndMixedOperationsMoreComplex_ReturnsCorrectResult()
+    {
+        var service = new RpnService();
+        var input = " 0 4 - 2 - 8 3 + 2 1.67 - / *6 2 4 2 / + / +";
+        
+        var result = service.СalculateRpn(input);
+
+        Assert.Equal(-198.5, result,0.000001);
+    }
+
+    [Fact]
+    public void CalculateRpn_WithDoubleNumbersAndMixedOperationsMoreComplex_ReturnsCorrectResult2()
+    {
+        var service = new RpnService();
+        var input = "0 2.3535 3.32 1.67 + 0 0.8 - / * - 6.48 0 2 - 20 0 2 - / + / +";
+
+        var result = service.СalculateRpn(input);
+
+        Assert.Equal(14.1399562, result, 0.000001);
+    }
+
+    [Fact]
+    public void CalculateRpn_WithIntengerNumbersAndMixedOperationsMoreComplex_ReturnsCorrectResult()
+    {
+        var service = new RpnService();
+        var input = "11 3 * 7 + 15 10 - / 16 17 + 20 12 - * - ";
+
+        var result = service.СalculateRpn(input);
+
+        Assert.Equal(-256, result, 0.000001);
+
+    [Fact]
+
     public void IsOperator_ShouldReturnTrue_ForValidOperators()
     {
         var symbol1 = '(';
@@ -164,5 +255,6 @@ public class RpnServiceTests
         var result = rpn.InfixNotationToRpn(input);
 
         Assert.Equal(expectedOutput, result);
+
     }
 }
---
+++ Calculator/Calculator/Services/Extensions/CharExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Services.Extensions
+{
+    public static class CharExtensions
+    {
+        public static bool IsOperator(this char c)
+        {
+            if ("()+-*/".Contains(c))
+                return true;
+            return false;
+        }
+    }
+}
